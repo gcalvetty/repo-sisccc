f0f0f1aee80a54e424000d49a72ecf16aa82b63f
gcalvetty <gcalvetty@gmail.com>
2019-01-09 20:32:18 -0400

    Modificar Rude

---
+++ resources/views/layouts_imprimir/view_rude_imprimir_2019.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html >
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<meta charset="utf-8" />
+</head>
+<body style="margin: 0;">
+<section>
+    <div id="p1" style="overflow: hidden; position: relative; background-color: white; width: 935px; height: 1540px;">
+        <!-- Begin page background -->
+        <div id="pg1Overlay" style="width:100%; height:100%; position:absolute; z-index:1; background-color:rgba(0,0,0,0); -webkit-user-select: none;"></div>
+        <div id="pg1" style="-webkit-user-select: none;"><object width="935" height="1540" data="1/1.svg" type="image/svg+xml" id="pdf1" style="width:935px; height:1540px; -moz-transform:scale(1); z-index: 0;"></object></div>
+    <!-- End page background -->
+    </div>
+</section> 
+
+<section>
+        <div id="p2" style="overflow: hidden; position: relative; background-color: white; width: 935px; height: 1540px;">
+                <!-- Begin page background -->
+                <div id="pg2Overlay" style="width:100%; height:100%; position:absolute; z-index:1; background-color:rgba(0,0,0,0); -webkit-user-select: none;"></div>
+                <div id="pg2" style="-webkit-user-select: none;">
+                    <object width="935" height="1540" data="2/2.svg" type="image/svg+xml" id="pdf2" style="width:935px; height:1540px; -moz-transform:scale(1); z-index: 0;"></object></div>
+                <!-- End page background -->
+        </div>
+</section>
+</body>
+</html>
